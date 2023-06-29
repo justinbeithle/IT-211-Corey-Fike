90a85e5c2e46c16c358a6ca3363104871ea85312
justinbeithle <justinbeithle@gmail.com>
2023-06-28 23:14:36 -0700

    week 1 excercise

---
+++ week-1/script.js
@@ -0,0 +1,9 @@
+let firstName = "Justin" 
+let lastName = "David"
+let yearsOfStudy = "1 year"
+
+let goal = "break into the tech industry and find a career I can move foward in and gain a high salary that can support the lifestlyle I want" 
+
+//let aboutMe = "My name is " + firstName + " " + lastName + "." + " " + "I have been at SCC for " + yearsOfStudy + " " + "and plan to " + goal + " " + "after completing my courses." 
+
+ 
