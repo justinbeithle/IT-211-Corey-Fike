33dede15e567052d8e0f9ca0f531abc410b846f3
justinbeithle <justinbeithle@gmail.com>
2023-07-07 23:07:07 -0700

    author

--- week-2/pig.js
+++ week-2/pig.js
@@ -1,3 +1,9 @@
+/*
+Pig Latin Translator 
+Written by: Justin David 
+Date: 7/7/2023
+*/
+
 /* - if a word begins with a vowel, the letter remains in place and 'way' is appended to the end of the word:
 apple --> appleway
 
@@ -11,114 +17,8 @@ grade --> adegray */
 
 If the user enters nothing or a single-letter word, your program should print a message that the input can't be translated.*/ 
 
-//Creating text input and output variables that connects to inner HTML
-/*const textInput = document.getElementById('textInput');
-const textOutput = document.getElementById('textOutput');
 
-textInput.addEventListener('keyup', (e) => {
-    const input =e.target.value;
-    textOutput.innerText = input;
-    console.log(input);
-
-});
-
-function pigLatinTranslator(str) {
-//error if empty field or one letter response 
-if (str == "" || str.length == 1) {
-    return "Incorrect Input: Please enter a word with more than one letter"
-}
-//creating an array of our vowels and a variable to input our pig latin string 
-let vowels = ['a', 'e', 'i', 'o', 'u']
-let newStr = "";
-  
-
-}
-*/
-/*
-const textInput = document.getElementById("textInput");
-const textOutput = document.getElementById("textOutput");
-const vowels = ["a", "i", "o", "u", "e"];
-const ending = "ay";
-
-//Method waits for key to be released 
-textInput.addEventListener("keyup", (e) => {
-  const input = e.target.value;
-  const output = input
-    .split(" ")
-    .map((word) => {
-      //if first letter is vowel then return with 'way' ending
-      if (vowels.includes(word[0])) { //set our vowels array at the top 
-        return word + "way";
-      }
-      //iterate through characters of word until we find a vowel
-      let consonantsPrefix = "";
-      for (let i = 0; i < word.length; i++) {
-        const char = word[i];
-        if (vowels.includes(char)) {
-          break;
-        }
-        consonantsPrefix += char;
-      }
-      return (
-        word.substring(consonantsPrefix.length) + consonantsPrefix + ending
-      );
-      //Iterate through characters of word
-    })
-    .join(" ");
-  textOutput.innerText = output;
-});
-*/
-
-/*
-const textInput = document.getElementById("textInput");
-const textButton = document.getElementById("textButton");
-const textOutput = document.getElementById("textOutput");
-const vowels = ["a", "i", "o", "u", "e"];
-const ending = "ay";
-
-// Function to convert the input text
-const convertText = () => {
-  const input = textInput.value.trim(); // Trim leading/trailing whitespace
-  if (input === "" || input.length === 1) {
-    textOutput.innerText = "Incorrect Input: Please enter a word with more than one letter";
-    return;
-  }
-
-  const input = textInput.value;
-  const output = input
-    .split(" ")
-    .map((word) => {
-      // if first letter is vowel then return with 'way' ending
-      if (vowels.includes(word[0])) {
-        return word + "way";
-      }
-      // iterate through characters of word until we find a vowel
-      let consonantsPrefix = "";
-      for (let i = 0; i < word.length; i++) {
-        const char = word[i];
-        if (vowels.includes(char)) {
-          break;
-        }
-        consonantsPrefix += char;
-      }
-      return (
-        word.substring(consonantsPrefix.length) +
-        consonantsPrefix +
-        ending
-      );
-    })
-    .join(" ");
-  textOutput.innerText = output;
-
-  if (output == "" || output.length == 1) {
-    return "Incorrect Input: Please enter a word with more than one letter"
-}
-};
-
-// Event listener for button click
-textButton.addEventListener("click", convertText);
-*/
-
+// pulling the id's we set in our html and assigned them a variable name to us for our JS
 const textInput = document.getElementById("textInput");
 const textButton = document.getElementById("textButton");
 const textOutput = document.getElementById("textOutput");
@@ -135,10 +35,11 @@ const convertText = () => {
     return;
   }
   
+//this splits the strings into an array of substrings and map iterates through it
   const output = input
     .split(" ")
     .map((word) => {
-      // if first letter is vowel then return with 'way' ending
+      // if first letter is vowel then return with original word and 'way' ending
       if (vowels.includes(word[0])) {
         return word + "way";
       }
@@ -152,19 +53,18 @@ const convertText = () => {
         consonantsPrefix += char;
       }
       return (
-        word.substring(consonantsPrefix.length) +
-        consonantsPrefix +
-        ending
+        word.substring(consonantsPrefix.length) + // this esentially gets a substring of original word and extracts the beggining consonants
+        consonantsPrefix + ending //where it is added to the end and "ay" is concatenated after 
       );
     })
     .join(" ");
-  textOutput.innerText = output;
+  textOutput.innerText = output; 
 };
 
 // Event listener for button click
-textButton.addEventListener("click", convertText);
+textButton.addEventListener("click", convertText);   //when button is clicked it calls onto our pig latin translator function
 
-// Event listener for Enter key press
+// Event listener for Enter key press and has it so enter key will work as well 
 textInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
